efdba1076f188f772ba5cd0ab00af30415dad905
daluamon <marcio.o.alves@hotmail.com>
2023-06-28 15:47:51 -0300

    Aplicação para estudo em Next 13 fix

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,6 @@ export default function Home() {
             <h2 className='text-black text-xl font-bold'>
               ⚠️ Opa, nenhum resultado encontrado
             </h2>
-            <p>{allCars?.message}</p>
           </div>
         )}
       </div>
